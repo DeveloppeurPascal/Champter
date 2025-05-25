2603df775fe27c6d17d24d4a344df88165580ff2
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-05-25 19:32:28 +0200

    renamed the v1.x project to allow opening it with the v2.x version

--- src-Champter/Champter_2021.dpr
+++ src-Champter/Champter_2021.dpr
@@ -1,4 +1,36 @@
-program Champter;
+﻿(* C2PP
+  ***************************************************************************
+
+  Champter
+
+  Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://champter.gamolf.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/Champter
+
+  ***************************************************************************
+  File last update : 2025-05-25T18:43:42.000+02:00
+  Signature : 1da76bfe720a7c82d39b6cef82a608fbefbb5eae
+  ***************************************************************************
+*)
+
+program Champter_2021;
 
 uses
   System.StartUpCopy,
